4c214674ba9dd5a7476c0c7b0bd13c6eaa69b032
NicholasScott <daymonforeman14@gmail.com>
2024-01-02 11:36:40 -0600

    Add initial GlowSteps project files
    This commit adds the initial set of files for the GlowSteps project. The files include the solution file, project file, a plugin class, and the project assets json file. Detailed configurations, package references and targets for the overall solution and individual projects are also included.

---
+++ GlowSteps/Plugin.cs
@@ -0,0 +1,10 @@
+﻿using BepInEx;
+
+namespace GlowSteps
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+    }
+}
---
+++ GlowSteps/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace GlowSteps
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "NicholaScott.LethalCompany.GlowSteps";
+        public const string PLUGIN_NAME = "Glow Steps";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
